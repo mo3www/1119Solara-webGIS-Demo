76b40a8667d746fdddb01224ce57da581d81c0fb
mo3www <linamy3893451@gmail.com>
2025-11-25 06:20:30 +0000

    new file:   GM_027.jpg 	modified:   pages/00_home.py

--- pages/00_home.py
+++ pages/00_home.py
@@ -1,38 +1,12 @@
 import solara
-import pandas as pd
 import plotly.express as px
-
-
-# 準備資料
-df = pd.DataFrame(
-    {"x": [1, 2, 3, 4], "y": [10, 11, 12, 13]}
-)
-fig = px.scatter(df, x="x", y="y", title="我的 Plotly 圖表")
 
 @solara.component
 def Page():
-    
-    solara.Markdown("### 水平排列 (Row)")
-
-    # 'with' 語法會自動將內部的所有元件放入 Row 容器
-    with solara.Row():
-        solara.Markdown("欄位 1", style={"width": "30%", "border": "1px solid red"})
-        solara.Markdown("欄位 2", style={"width": "70%", "border": "1px solid blue"})
-
-    solara.Markdown("---")
-    
-    solara.Markdown("### 垂直排列 (Column)")
-    
-    # 'with' 語法會自動將內部的所有元件放入 Column 容器
-    with solara.Column():
-        solara.Markdown("這個元件在 Row 的下面")
-        solara.Markdown("這個元件在更下面")
-    
-    solara.Markdown("### 這是另一個置中的垂直排列容器 (Column)")
-
     # 'with' 語法會自動將內部的所有元件放入 Column 容器
     with solara.Column(align="center"):
-        solara.Markdown("## 顯示 DataFrame")
-        solara.DataFrame(df)
-        solara.Markdown("## 顯示 Plotly 圖表")
-        solara.FigurePlotly(fig)
+        solara.Markdown("## 馬太鞍溪簡介")
+        solara.Markdown("---")
+        solara.Markdown("馬太鞍溪發源自丹大山北邊，是花蓮溪的支流之一。關門古道越過東段最高點的倫太文山之後，先沿稜下降到倫太文西鞍，之後便往下繞過兩個大山溝，再沿西北向稜線下降到海拔1800公尺處的馬太鞍溪上游，下溪處為一寬闊平坦地，為清軍駐紮之營盤，有七株超過15公尺高的紅檜，故稱為「巨木營盤址」，古道過溪之後，便一路陡上至中央山脈主稜。此處的溪水清澈見底，水深及膝，溪中處處是白色大石，加上兩岸林木蓊鬱，十分清幽。")
+        solara.Image(image=f"GM_027.jpg", width="50%")
+
